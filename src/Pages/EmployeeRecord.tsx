bbbd90e7ad80d5a445fa8f6c216178d212fe6701
IrfanShah <71603574+DeveloperIrkhan@users.noreply.github.com>
2021-08-03 11:17:21 +0500

    All changes

--- src/Pages/EmployeeRecord.tsx
+++ src/Pages/EmployeeRecord.tsx
@@ -1,88 +1,167 @@
 import React from 'react'
 
-interface Props {}
+interface Props { }
 
+interface EmployeeDetails {
+    Name: string,
+    Email: string,
+    Phone: string,
+    Address: string
+}
 interface State {
-    EmployeeDetails:object,
-    Name:string,
-    Email:string,
-    Phone:string
+    Employee: EmployeeDetails;
+    EmployeeList: EmployeeDetails[];
+    Valid: boolean;
 }
 
 export class EmployeeRecord extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            Name:"",
-            Email:"",
-            Phone:"",
-            EmployeeDetails:[]
+            Employee: Object(),
+            EmployeeList: [],
+            Valid: false,
         };
+        this.PushData = this.PushData.bind(this);
+        this.validationCheck=this.validationCheck.bind(this);
     }
 
-    PushData (){
-        let name=this.state.Name;
-        let email= this.state.Email;
-        let phone = this.state.Phone;
-        let employeedetails= this.state.EmployeeDetails;
-
-        if(!(employeedetails===null)){
-            employeedetails=[];
+    PushData() {
+        let employee = this.state.Employee;
+        let employeelist = this.state.EmployeeList;
+        if (!(employeelist)) {
+            employeelist = [];
         }
-
+        employeelist = ([...employeelist, { ...employee }]);
+        employee.Name = "";
+        employee.Email = "";
+        employee.Phone = "";
+        employee.Address = "";
+        this.setState({ EmployeeList: employeelist })
+    }
+    validationCheck() {
+        let Emp = this.state.Employee;
+        let Name = Emp.Name;
+        let Email = Emp.Email;
+        let Phone = Emp.Phone;
+        let Address = Emp.Address;
+        if (Name != "" || Email != "" || Phone != "" || Address != "") {
+            this.setState({ Valid: true })
+            this.PushData();
+        }
+        else {
+            this.setState({ Valid: false })
+        }
     }
     render() {
-        const {Name , Email , Phone , EmployeeDetails }= this.state;
-        return( 
-        <div className="bg-light-gray">
-            <div className="col-6 m-auto p-5">
-            <input 
-                        type="text" 
-                        className="form-control"
-                        value={Name}
-                        placeholder="Please Enter Your Name"
-                        onChange={ (e)=> {
-                            this.setState({Name: e.target.value})
-                        }}/>
+        const { Valid } = this.state;
+        let Employee = this.state.Employee;
+        let EmployeeList = this.state.EmployeeList
+        return (
+            <div className="bg-light-gray">
+                <div className="col-6 m-auto p-5">
+                    <div className={`${Valid === true && Employee.Name === "" ? "requiredbox active" : ""}`}>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={Employee.Name}
+                            placeholder="Please Enter Your Name"
+                            onChange={(e) => {
+                                Employee.Name = e.target.value
+                                this.setState({ Employee })
+                            }} />
+                        <h6 className="required py-1" style={{float:'left'}} >
+                            <img src="/alert.svg" width="20px" alt="" /> Your Name Required
+                        </h6>
+                    </div>
                     <br />
-                    <input 
-                        type="text" 
+                    <div className={`${Valid === true && Employee.Email === "" ? "requiredbox active" : ""}`}>
+                    <input
+                        type="text"
                         className="form-control"
-                        value={Email}
+                        value={Employee.Email}
                         placeholder="Please Enter Your Email"
-                        onChange={ (e)=> {
-                            this.setState({Email: e.target.value})
-                        }}/>
+                        onChange={(e) => {
+                            Employee.Email = e.target.value
+                            this.setState({ Employee })
+                        }} />
+                        <h6 className="required py-1" style={{float:'left'}} >
+                            <img src="/alert.svg" width="20px" alt="" /> Your Email Required
+                        </h6>
+                        </div>
                     <br />
-                    <input 
-                        type="text" 
+                    <div className={`${Valid === true && Employee.Phone === "" ? "requiredbox active" : ""}`}>
+                    <input
+                        type="text"
                         className="form-control"
-                        value={Phone}
+                        value={Employee.Phone}
                         placeholder="Please Enter Your Phone"
-                        onChange={ (e)=> {
-                            this.setState({Phone: e.target.value})
-                        }}/>
+                        onChange={(e) => {
+                            Employee.Phone = e.target.value
+                            this.setState({ Employee })
+                        }} />
+                        <h6 className="required py-1" style={{float:'left'}} >
+                            <img src="/alert.svg" width="20px" alt="" /> Your Phone Required
+                        </h6>
+                    </div>
                     <br />
-                    <button
-                        onClick={this.PushData}
-                         className="btn btn-outline-success col-3">Save</button>
+                    <div className={`${Valid === true && Employee.Address === "" ? "requiredbox active" : ""}`}>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={Employee.Address}
+                        placeholder="Please Enter Your Address"
+                        onChange={(e) => {
+                            Employee.Address = e.target.value
+                            this.setState({ Employee })
+                        }} />
+                        <h6 className="required py-1" style={{float:'left'}} >
+                            <img src="/alert.svg" width="20px" alt="" /> Your Address Required
+                        </h6>
+                    </div>
+                    <br />
+                    
+                </div>
+               <div>
+               <button
+                        onClick={()=>{
+                            this.validationCheck();
+                        }}
+
+                        // className={`${Valid ? "btn btn-outline-success mb-5 col-3 disabled" :"btn btn-outline-success mb-5 col-3"}`}
+                        className="btn btn-outline-success mb-5 col-3"
+                        >
+                        Save</button>
+               </div>
+                <div className="container">
+                    <table className="table table-bordered">
+                        <thead className="bg-danger text-white">
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Phone No</th>
+                                <th>Address</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {EmployeeList?.map((list, key) => {
+                                return (
+                                    <tr>
+                                        <td>{key + 1}</td>
+                                        <td>{list.Name}</td>
+                                        <td>{list.Email}</td>
+                                        <td>{list.Phone}</td>
+                                        <td>{list.Address}</td>
+                                    </tr>
+
+                                )
+                            })}
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div className="container">
-                <table className="table table-bordered">
-                    <thead className="bg-danger text-white">
-                        <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Phone No</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr></tr>
-                    </tbody>
-                </table>
-            </div>
-        </div> 
-        ) 
+        )
     }
 }
